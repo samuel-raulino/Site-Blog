8f16cd03c289a68836ec2f057dea9f3a223fb636
samuel-raulino <113915653+samuel-raulino@users.noreply.github.com>
2024-07-04 08:53:27 -0300

    Initial commit

---
+++ blog beta-1/bandas/site.js
@@ -0,0 +1,4 @@
+
+function buttonClick() {
+    window.open("http://127.0.0.1:5500/")
+}
---
+++ blog beta-1/inicio/site.js
@@ -0,0 +1,12 @@
+
+function buttonClick() {
+    window.open("http://127.0.0.1:5500/bandas/index__bandas.html")
+}
+
+function buttonClickInicio() {
+    window.open("http://127.0.0.1:5500/bandas/emprodu%C3%A7%C3%A3o.html")
+}
+
+function buttonClickTeste() {
+    window.open("http://127.0.0.1:5500/bandas/emprodu%C3%A7%C3%A3o.html")
+}
